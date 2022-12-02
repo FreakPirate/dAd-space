d201099959be5b36ae5ae33b35e47739a6c6a1bb
muddhitbaid <muddhit@rocketium.com>
2022-12-03 00:49:52 +0530

    fix: added layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import { Menu } from "antd";
-import styled from "styled-components";
-
+import { Layout, Menu } from 'antd';
+import styled from 'styled-components';
 
 function App() {
-  const items = [
-    { label: 'item 1', key: 'item-1' }, // remember to pass the key prop
-    { label: 'item 2', key: 'item-2' }, // which is required
-    {
-      label: 'sub menu',
-      key: 'submenu',
-      children: [{ label: 'item 3', key: 'submenu-item-1' }],
-    },
-  ];
-  return (
-    <StyledApp>
-      <Menu
-        theme={'dark'}
-        style={{
-          width:256,
-          height:'100vh',
-        }}
-        mode="inline"
-        items={items}
-      />
-    </StyledApp>
-  );
+	const { Header, Content, Footer, Sider } = Layout;
+
+	const items = [
+		{ label: 'item 1', key: 'item-1' }, // remember to pass the key prop
+		{ label: 'item 2', key: 'item-2' }, // which is required
+	];
+	return (
+		<StyledApp>
+			<Layout>
+				<Sider style={{ width: 256, height: '100vh' }}>
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" items={items} />
+				</Sider>
+				<Layout>
+					<Content>
+						<div className="site-layout-background">content</div>
+					</Content>
+				</Layout>
+			</Layout>
+		</StyledApp>
+	);
 }
 
 const StyledApp = styled.div`
-    background-color: rgb(23 23 23/1);
-    height:100vh;
-    width:100vw;
-    color: #ffffff;
+	background-color: rgb(23 23 23/1);
+	height: 100vh;
+	width: 100vw;
+	color: #ffffff;
 `;
 export default App;
