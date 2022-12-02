52c028de0d2425546a7c6a68613b695f956e482f
muddhitbaid <muddhit@rocketium.com>
2022-12-03 00:27:40 +0530

    chore: added a menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Menu } from "antd";
 import styled from "styled-components";
 
 
 function App() {
+  const items = [
+    { label: 'item 1', key: 'item-1' }, // remember to pass the key prop
+    { label: 'item 2', key: 'item-2' }, // which is required
+    {
+      label: 'sub menu',
+      key: 'submenu',
+      children: [{ label: 'item 3', key: 'submenu-item-1' }],
+    },
+  ];
   return (
     <StyledApp>
-      
+      <Menu
+        theme={'dark'}
+        style={{
+          width:256,
+          height:'100vh',
+        }}
+        mode="inline"
+        items={items}
+      />
     </StyledApp>
   );
 }
