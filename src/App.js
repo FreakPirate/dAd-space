2f8da9252cf132160ee80a949513ca42abe3fbb2
muddhitbaid <muddhit@rocketium.com>
2022-12-02 23:13:44 +0530

    chore: boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      
+    </StyledApp>
   );
 }
 
+const StyledApp = styled.div`
+    background-color: rgb(23 23 23/1);
+    height:100vh;
+    width:100vw;
+    color: #ffffff;
+`;
 export default App;
