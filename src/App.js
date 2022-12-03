764a27ce84fbeec74e778391fe7ae875c8bc7b73
Nitin Gupta <nitin@rocketium.com>
2022-12-03 16:29:43 +0530

    feat: ability to disconnect and logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Layout, Menu } from 'antd';
+import { Tag, Layout, Menu, Tooltip } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { LOGO } from './constants';
 import UploadContainer from './components/UploadContainer';
 import UploadedContainer from './components/UploadedContainer';
 import PastAdsList from './components/PastAdsList';
+const { Content, Sider } = Layout;
 
-function App() {
-	const { Content, Sider } = Layout;
-
+const App = ({handleDisconnect, userDetails}) => {
 	const items = [
 		{ label: 'New Ad', key: 'newAd' },
 		{ label: 'Past Ads', key: 'pastAds' },
@@ -21,6 +21,14 @@ function App() {
 
 	const handleMenuItemSelect = ({ key }) => {
 		setSelectedView(key);
+	};
+
+	const handleLogout = () => {
+		handleDisconnect();
+	};
+
+	const getUserId = (userId) => {
+		return `${userId?.substring(0, 4)}....${userId?.substring(userId?.length - 4)}`;
 	};
 
 	const getRightSideContent = (selectedView) => {
@@ -37,7 +45,7 @@ function App() {
 	return (
 		<StyledApp>
 			<Layout>
-				<Sider width={250} style={{ height: '100vh', background: '#171717', boxShadow: '0.2px 0px #8d9093' }}>
+				<StyledSider width={250}>
 					<AppLogo className="logo">
 						<LogoWrapper src={LOGO} alt="dAd Space" />
 					</AppLogo>
@@ -49,7 +57,19 @@ function App() {
 						selectedKeys={[selectedView]}
 						onClick={handleMenuItemSelect}	
 					/>
-				</Sider>
+					<UserProfile>
+						<MetamaskLogo src='https://imgs.search.brave.com/F0igGqGdXUAx5UeqVyb1L-99SHazMKwsTRXdK0GPEus/rs:fit:1080:1080:1/g:ce/aHR0cDovL3d3dy5l/dGhlcmxhbWJvcy5p/by9pbWcvbWV0YW1h/c2sucG5n' alt='User:' />
+						<ConnectedTag>
+							<Tag color="lime">
+								connected
+							</Tag>
+						</ConnectedTag>
+						<Tooltip title={userDetails} placement='topLeft'>
+							<UserIdText>{getUserId(userDetails)}</UserIdText>
+						</Tooltip>
+						<StyledLogout onClick={handleLogout}/>
+					</UserProfile>
+				</StyledSider>
 				<Layout style={{height: '100vh', background: 'rgb(25, 25, 25)'}}>
 					<Content style={{display: 'flex'}}>
 						{getRightSideContent(selectedView)}
@@ -58,7 +78,25 @@ function App() {
 			</Layout>
 		</StyledApp>
 	);
-}
+};
+
+export default App;
+
+const StyledSider = styled(Sider)`
+	height: 100vh;
+	background: #171717 !important;
+	boxShadow: 0.2px 0px #8d9093 !important;
+
+	.ant-layout-sider-children {
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
+const MetamaskLogo = styled.img`
+	width: 35px;
+	height: 35px;
+`;
 
 const StyledMenu = styled(Menu)`
 	background: #171717;
@@ -89,5 +127,27 @@ const AppLogo = styled.div`
 const LogoWrapper = styled.img`
 	width: 90%;
 `;
-
-export default App;
+const UserProfile = styled.div`
+	margin-top: auto;
+	padding: 10px;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	align-content: center;
+	justify-content: flex-start;
+	align-items: center;
+`;
+const UserIdText = styled.div`
+	margin: 10px 0px 0px 5px;
+	font-size: 12px;
+`;
+const ConnectedTag = styled.div`
+	margin-top: 10px;
+	margin-left: 5px;
+`;
+const StyledLogout = styled(LogoutOutlined)`
+	margin-top: 10px;
+	margin-left: 10px;
+	color: #F73859;
+	font-size: 20px;
+`;
