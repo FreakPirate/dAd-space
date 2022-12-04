fc11033046b078021a11dddaecaa180afa66207a
Yashank <46098062+Yashank18@users.noreply.github.com>
2022-12-04 00:12:31 +0000

    fix: adds only when adress available

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App(props) {
+  const [url1, setUrl1] = useState('');
+  const [url2, setUrl2] = useState('');
   useEffect(() => {
     if (props?.userId?.includes('0x')) {
-      console.log('Done')
+      setUrl1('https://dad-space.s3.ap-south-1.amazonaws.com/Variant-1_800x16001670084659339.png');
+      setUrl2('https://dad-space.s3.ap-south-1.amazonaws.com/Variant-1-copy-1_800x16001670084686480.png');
+    } else {
+      setUrl1('');
+      setUrl2('');
     }
     // Update the document title using the browser API
   },[props?.userId]);
@@ -22,8 +28,7 @@ function App(props) {
       </header>
       <div className="mainBody">
         <div id="dad-vertical-add">
-          <img src='https://dad-space.s3.ap-south-1.amazonaws.com/Variant-1_800x16001670084659339.png'></img>
-
+          {url1 !== '' ? <img src={url1}></img> : null}
         </div>
         <div style={{"displaye": "flex"}}>
           <img src={logo} className="App-logo" alt="logo" />
@@ -32,7 +37,7 @@ function App(props) {
           </p>
         </div>
         <div id="dad-vertical-add">
-          <img src="https://dad-space.s3.ap-south-1.amazonaws.com/Variant-1-copy-1_800x16001670084686480.png"></img>
+         {url2 !== '' ? <img src={url2}></img> : null }
         </div>
       </div>
     </div>
